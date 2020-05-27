6ec5f43b1f1fc78f29042ba79759302428d35309
Pavel Matsenko <pavelmatsenko@gmail.com>
2020-05-27 19:13:09 +0300

    added fonts, colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import './css/main.css';
+
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   return (
     <div>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+
+const Header = () => {
+    return <header className={styles.test}>ferfe</header>
+};
+
+export default Header;
