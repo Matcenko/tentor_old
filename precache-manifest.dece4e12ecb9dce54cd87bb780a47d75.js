aa8d7762a26e8900e03c5e3786ac31341e1b813b
Pavel Matsenko <pavelmatsenko@gmail.com>
2020-06-01 23:07:57 +0300

    Updates

---
+++ precache-manifest.dece4e12ecb9dce54cd87bb780a47d75.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1e0d59d0782e6e3517681bff01e469",
+    "url": "/tentor/index.html"
+  },
+  {
+    "revision": "8b12ebc3394b8f60699b",
+    "url": "/tentor/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "df90f49a862b86188903",
+    "url": "/tentor/static/css/main.682fb107.chunk.css"
+  },
+  {
+    "revision": "8b12ebc3394b8f60699b",
+    "url": "/tentor/static/js/2.4d0e45b0.chunk.js"
+  },
+  {
+    "revision": "4d7749ba1ea6a5029026f2621a685668",
+    "url": "/tentor/static/js/2.4d0e45b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df90f49a862b86188903",
+    "url": "/tentor/static/js/main.1e12db74.chunk.js"
+  },
+  {
+    "revision": "b2850ab4749dc0989a72",
+    "url": "/tentor/static/js/runtime-main.0e187cee.js"
+  },
+  {
+    "revision": "b25154f00336ae1fae359cbec0b28973",
+    "url": "/tentor/static/media/Alef-Bold.b25154f0.ttf"
+  },
+  {
+    "revision": "c5d54929c418e69868b42a4d75063958",
+    "url": "/tentor/static/media/Alef-Regular.c5d54929.ttf"
+  },
+  {
+    "revision": "a77779f001a1944ec7ca5be4be3c34db",
+    "url": "/tentor/static/media/Athelas-Bold.a77779f0.ttf"
+  },
+  {
+    "revision": "203ca707d5875159f71d2aff42401749",
+    "url": "/tentor/static/media/Athelas-Regular.203ca707.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/tentor/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/tentor/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "6d320f4954f556259424ac6899c42f6e",
+    "url": "/tentor/static/media/arrow.6d320f49.svg"
+  },
+  {
+    "revision": "cc8eeaa72e673ef75140516f969fc1b5",
+    "url": "/tentor/static/media/binoculars.cc8eeaa7.svg"
+  },
+  {
+    "revision": "453fea76c5d2455f94324d9c15a8c29d",
+    "url": "/tentor/static/media/boat_slide2.453fea76.png"
+  },
+  {
+    "revision": "2128bd550e952cc0eb7d1fbd638f6939",
+    "url": "/tentor/static/media/camping_slide3.2128bd55.png"
+  },
+  {
+    "revision": "33837402de842fe143600d08275703e3",
+    "url": "/tentor/static/media/cart.33837402.svg"
+  },
+  {
+    "revision": "2c8769730bf5c2d9dc9dd34968488208",
+    "url": "/tentor/static/media/category1.2c876973.png"
+  },
+  {
+    "revision": "fc84d9c360aa79e76d697c2ae1af3b4b",
+    "url": "/tentor/static/media/category2.fc84d9c3.png"
+  },
+  {
+    "revision": "17789f2b72b685037611c424137d9f46",
+    "url": "/tentor/static/media/facebook.17789f2b.svg"
+  },
+  {
+    "revision": "0f1822520e9a207c41705c17a3c22ded",
+    "url": "/tentor/static/media/helm.0f182252.svg"
+  },
+  {
+    "revision": "3e85982a9012f5fcbf73d9c8570be1e0",
+    "url": "/tentor/static/media/logo.3e85982a.svg"
+  },
+  {
+    "revision": "4c0e6b8956e76523fd9953e4b15d55a3",
+    "url": "/tentor/static/media/preference.4c0e6b89.svg"
+  },
+  {
+    "revision": "1cf9b186964ecc1ffb0fa1e682290a8e",
+    "url": "/tentor/static/media/productAvatar.1cf9b186.png"
+  },
+  {
+    "revision": "5b3da9f208dce3104733d2cebb28a8bc",
+    "url": "/tentor/static/media/tackle_slide1.5b3da9f2.png"
+  }
+]);
